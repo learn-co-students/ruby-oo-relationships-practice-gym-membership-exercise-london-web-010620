3e06cd5b8fe4a1b5d6b7d5bb67af1e583c682a94
RikBala <rik280491@googlemail.com>
2020-01-14 17:10:45 +0000

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,23 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self 
   end
+
+  def self.all
+    @@all 
+    
+  end 
+
+  
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,17 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+g1 = Gym.new("Adam")
+g2 = Gym.new("Eve")
+g3 = Gym.new("John")
 
+l1 = Lifter.new("Bob", 200)
+l2 = Lifter.new("Cam", 300)
+l3 = Lifter.new("David", 500)
+
+m1 = Membership.new(50, g1, l1)
+m2 = Membership.new(70, g2, l2)
+m3 = Membership.new(55, g3, l3)
 binding.pry
 
 puts "Gains!"
