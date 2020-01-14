826072813122ce9924d92ef74942af8e9a807799
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-14 17:57:01 +0000

    done

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,43 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships_for_lifter
+    Membership.all.select {|mem| mem.lifter == self}
+  end
+
+  def gyms_for_lifter
+    memberships_for_lifter.select {|mem| mem.gym.name}
+    
+  end
+
+  def self.average_lift_for_all
+    total_lifts = Lifter.all.map {|lifter| lifter.lift_total}.sum
+    total_lifts / @@all.length
+  end
+
+  def lifter_total_mem_cost
+    memberships_for_lifter.map {|mem| mem.cost}.sum
+  end
+
+  def new_mem(gym,mem_cost)
+    Membership.new(gym,mem_cost,self)
+  end
+
+  
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,22 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+gym1 = Gym.new("gym_A")
+gym2 = Gym.new("gym_B")
+gym3 = Gym.new("gym_C")
+
+lifter1 = Lifter.new("lifter_AA", 500)
+lifter2 = Lifter.new("lifter_BB", 600)
+lifter3 = Lifter.new("lifter_CC", 700)
+
+mem1 = Membership.new(20,gym1,lifter1)
+mem2 = Membership.new(30,gym2,lifter2)
+mem3 = Membership.new(40,gym3,lifter3)
+mem4 = Membership.new(50,gym2,lifter1)
+
+
+
+
 binding.pry
 
 puts "Gains!"
